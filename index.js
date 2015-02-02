302cbdd3e30321c3f4c49f956f3aec68720ab17e
Shawn Allen <shawn.allen@gsa.gov>
2015-02-01 23:13:06 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var request = require("request"),
+    async = require("async"),
+    extend = require("util-extend");
+
+module.exports = enhance;
+enhance.url = enhanceURL;
+
+function enhance(data, baseUrl, done) {
+  async.map(data.apis, function(api, next) {
+    var apiUrl = baseUrl + api.path;
+    request(apiUrl, function(error, response, body) {
+      if (error) return next(error);
+      extend(api, JSON.parse(body));
+      ["swaggerVersion", "apiVersion"].forEach(function(k) {
+        delete api[k];
+      });
+      next(null, api);
+    });
+  }, function(error, apis) {
+    done(error, data);
+  });
+}
+
+function enhanceURL(url, done) {
+  var baseUrl = url.match(/\.json$/)
+    ? url.slice(0, url.lastIndexOf("/"))
+    : url;
+  request(url, function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      return done("Unable to load '" + url + "': " + error);
+    }
+    var data = JSON.parse(body);
+    enhance(data, baseUrl, function(error) {
+      if (error) return done("Unable to enhance data: " + error);
+      return done(null, data);
+    });
+  });
+}
